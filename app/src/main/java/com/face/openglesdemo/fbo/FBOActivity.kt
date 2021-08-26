a05754d474b0233875c888996d04e5433fe64c1a
sheng.tang@iim.ltd <ts123456>
2021-08-26 15:32:45 +0800

    FBO

--- app/src/main/java/com/face/openglesdemo/fbo/FBOActivity.kt
+++ app/src/main/java/com/face/openglesdemo/fbo/FBOActivity.kt
@@ -2,6 +2,7 @@ package com.face.openglesdemo.fbo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.face.openglesdemo.R
 
 class FBOActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
